5924a61c2ac0816692e741de492b6691c741d54d
amandasgm <144074360+amandasgm@users.noreply.github.com>
2024-04-21 17:53:33 -0300

    admin de app_voluntarios configurado

--- app_voluntarios/admin.py
+++ app_voluntarios/admin.py
@@ -1,3 +1,10 @@
+
 from django.contrib import admin
+from app_voluntarios.models import Voluntariado
 
 # Register your models here.
+@admin.register(Voluntariado)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'email', 'celular', 'endereço', 'cpf', 'profissão']
+    search_fields = ['nome', 'email', 'celular', 'endereço', 'cpf', 'profissão']
+    list_filter = ['ativo', 'status']
